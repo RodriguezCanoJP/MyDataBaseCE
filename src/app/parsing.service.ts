39ec4f2f2ad67cf6b5c27b37da3ff4e94de94614
RodriguezCanoJP <jp99@estudiantec.cr>
2023-05-26 18:20:54 -0600

    lee archivo xml y retorna arreglo

--- src/app/parsing.service.ts
+++ src/app/parsing.service.ts
@@ -1,9 +1,48 @@
 import { Injectable } from '@angular/core';
+import * as xml2js from 'xml2js'; 
+import { HttpClient, HttpHeaders } from '@angular/common/http'; 
 
 @Injectable({
   providedIn: 'root'
 })
 export class ParsingService {
 
-  constructor() { }
+  constructor(private _http: HttpClient) {}
+
+  loadXML(){  
+    return this._http.get('/assets/objeto.xml',  
+      {  
+        headers: new HttpHeaders()  
+          .set('Content-Type', 'text/xml')  
+          .append('Access-Control-Allow-Methods', 'GET')  
+          .append('Access-Control-Allow-Origin', '*')  
+          .append('Access-Control-Allow-Headers', "Access-Control-Allow-Headers, Access-Control-Allow-Origin, Access-Control-Request-Method"),  
+        responseType: 'text'  
+      })   
+  }  
+
+  parseXML(data: string) {  
+    return new Promise(resolve => {  
+      var k: Object,  
+        arr: any = [],  
+        parser = new xml2js.Parser(  
+          {  
+            trim: true,  
+            explicitArray: true  
+          });  
+      parser.parseString(data, function (err, result) { 
+        const storeString : string = Object.keys(result)[0];//Main string
+        const store = result[storeString];//Store object
+        const groupString : string = Object.keys(store)[0];//Store obj string
+        const objs = store[groupString];//Objects array
+
+        objs.forEach((obj: { [s: string]: unknown; } | ArrayLike<unknown>) =>{
+          var listed = Object.entries(obj);//lsit of key-value pairs
+          arr.push(listed);
+        })
+      
+        resolve(arr);  
+      });  
+    });  
+  }  
 }

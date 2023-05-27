9c1e5ff93a72cce2b46e80722965c4279c1339d9
RodriguezCanoJP <jp99@estudiantec.cr>
2023-05-26 18:51:36 -0600

    muestra arreglo en tabla

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { XmltableComponent } from './xmltable/xmltable.component';
+import { ParsingService } from './parsing.service';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,7 @@ import { XmltableComponent } from './xmltable/xmltable.component';
     AppRoutingModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [ParsingService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/xmltable/xmltable.component.ts
+++ src/app/xmltable/xmltable.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ParsingService } from '../parsing.service';
 
 @Component({
   selector: 'app-xmltable',
   templateUrl: './xmltable.component.html',
   styleUrls: ['./xmltable.component.css']
 })
-export class XmltableComponent {
+export class XmltableComponent implements OnInit{
+  public xmlItems: any;
 
+  constructor(private service: ParsingService){}
+
+  ngOnInit(): void {
+    this.service.loadXML().subscribe(data => {  
+      this.service.parseXML(data)  
+        .then(parsed_data => {  
+           this.xmlItems =  parsed_data;  
+        });  
+    });
+  }
 }

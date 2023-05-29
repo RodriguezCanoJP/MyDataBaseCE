19b07ee092b389a7d44669e92cd1bd2f26a58d86
RodriguezCanoJP <jp99@estudiantec.cr>
2023-05-29 17:40:32 -0600

    guarda el input del usuario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,15 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'mydatabase';
+
+  public name = '';
+
+  public onValueChange(event: Event): void {
+    const value = (event.target as any).value;
+    this.name = value;
+  }
+
 }

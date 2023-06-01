89c460af9fdd1ebbd4f250161db785a8e5348619
RodriguezCanoJP <jp99@estudiantec.cr>
2023-06-01 13:37:16 -0600

    agrega biblioteca de angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import {MatTableModule} from '@angular/material/table';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,7 +16,8 @@ import { ParsingService } from './parsing.service';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    MatTableModule
   ],
   providers: [ParsingService],
   bootstrap: [AppComponent]

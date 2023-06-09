79733a7f0d96309a4e27617741c5a0e3707f359a
RodriguezCanoJP <jp99@estudiantec.cr>
2023-06-09 11:43:14 -0600

    cambios de ventana

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { MatTableModule } from '@angular/material/table';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatCardModule } from '@angular/material/card';
-import { MatSelectModule } from '@angular/material/select'
-import { FormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
 
 import { AppComponent } from './app.component';
 import { XmltableComponent } from './xmltable/xmltable.component';
@@ -32,9 +35,12 @@ import { SignupComponent } from './signup/signup.component';
     HttpClientModule,
     MatTableModule,
     FormsModule,
+    ReactiveFormsModule,
     MatFormFieldModule,
     MatCardModule,
-    MatSelectModule
+    MatSelectModule,
+    MatToolbarModule,
+    FlexLayoutModule
   ],
   providers: [ParsingService, ScriptService],
   bootstrap: [AppComponent]

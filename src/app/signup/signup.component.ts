f24c3689f37eb063e48c13b9ee28d3fc522e2226
Striker2785 <meneito200337@gmail.com>
2023-06-12 18:51:53 -0600

    Actualizando el huffman code

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { HuffmanService } from '../huffman.service';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
+
 export class SignupComponent {
   Roles: any = ['Admin', 'Author', 'Reader'];
   constructor(){}
 
+  password!: string;
+  myInstance!: HuffmanService;
+
+  createHuffman(): void {
+
+    this.myInstance = new HuffmanService();
+    this.myInstance.generateHuffmanTree(this.password); // generate
+    this.myInstance.inputText = this.password; // input
+    this.myInstance.encode(); // encode
+    console.log(this.myInstance.encodedText); // encode
+  }
 }
+
+
